588ddeef5060fb6f1c35733879a2acea538a8fc1
Raul Knab <raul.knab@gmx.de>
2025-03-24 00:35:01 +0100

    asdasd

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CharacterTable from "./CharacterTable";
 function App() {
   return (
     <div className="container">
-      <h1>SKS Liste Echoes of Madness</h1>
+      <h1>SKS Liste Echoes of Madness </h1>
       <CharacterTable />
     </div>
   );
