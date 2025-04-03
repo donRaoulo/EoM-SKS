24fe85e17ff71a7900344891b593bf8445da3dbd
Raul Knab <raul.knab@gmx.de>
2025-04-04 01:01:26 +0200

    remove logo

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import CharacterTable from "./CharacterTable";
 function App() {
   return (
 <div className="container" style={{textAlign: "center", maxWidth: "900px", margin: "auto" }}>
-  <img
-    src={process.env.PUBLIC_URL + "/Logo.jpeg"}
-    alt="Logo"
-    style={{ maxWidth: "400px", marginBottom: "20px" }}
-  />
-  <h1>SKS Liste</h1>
+  <h1>EoM SKS Liste</h1>
   <CharacterTable />
 </div>
 
